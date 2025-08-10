810aa7834879f4ede92255a98225f85e15a91451
sruthidacherla <sruthi.dacherla@gmail.com>
2025-08-09 23:09:46 -0400

    Created My Skills section

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -4,6 +4,7 @@ import NavigationBar from './components/NavigationBar';
 import Hero from './components/hero';
 import About from './components/About';
 import Projects from './components/Projects';
+import Skills from './components/Skills';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Hero />
         <About />
         <Projects />
+        <Skills />
     </div>
   )
 }
---
+++ my-project/src/components/Skills.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const skills = [
+  {
+    id: 1,
+    title: "Full-Stack Development",
+    description: "ReactJS, Tailwind CSS, Node.js, SQL, MongoDB, Git, Docker, REST APIs",
+  },
+  {
+    id: 2,
+    title: "UI/UX Design",
+    description: "Figma, Responsive Design, HTML, CSS, JavaScript",
+  },
+  {
+    id: 3,
+    title: "AI & Machine Learning",
+    description: "Python, R, Streamlit, Data Analytics",
+  },
+  {
+    id: 4,
+    title: "Geospatial & Simulation Modeling",
+    description: "Google Earth Engine, Remote Sensing, GIS, SIBBORK Model, Large-Scale Simulations",
+  },
+];
+const Skill = () => {
+  return (
+    <div className="bg-black text-white py-20" id="skill">
+      <div className="container mx-auto px-8 md:px-16 lg:px-24">
+        <h2 className="text-4xl font-bold mb-10 text-center">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-400 via-purple-400 to-blue-500">
+            My Skills
+          </span>
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {skills.map((skill) => (
+            <div
+              key={skill.id}
+              className="bg-gray-800 px-6 pb-6 rounded-lg hover:shadow-lg transform 
+              transition-transform duration-300 hover:scale-105"
+            >
+              <h3 className="mt-2 text-2xl font-bold text-transparent bg-clip-text 
+              bg-white">
+                {skill.title}
+              </h3>
+              <p className="mt-2 text-gray-300">{skill.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skill;
