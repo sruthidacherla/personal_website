f9a214b47e5337119c4f75595b65bcfca3e4146d
sruthidacherla <sruthi.dacherla@gmail.com>
2025-11-18 20:00:54 -0500

    resume

--- my-project/src/components/hero.jsx
+++ my-project/src/components/hero.jsx
@@ -31,12 +31,15 @@ const Hero = () => {
           >
             LinkedIn
           </button>
-          <button
+          <a
+            href="/resume.pdf"  
+            target="_blank"
+            rel="noopener noreferrer"
             className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
-                       transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
+                      transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full inline-block"
           >
             Resume
-          </button>
+          </a>
         </div>
 
       </div>
